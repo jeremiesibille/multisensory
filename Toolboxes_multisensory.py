163ace37dbe983793c057f28c0eab13dcef9a991
jeremiesibille <chezjems@gmail.com>
2023-08-17 17:58:20 +0200

    Update Toolboxes_multisensory.py
    2.0

--- Toolboxes_multisensory.py
+++ Toolboxes_multisensory.py
@@ -5,10 +5,10 @@ These are triggered on RZ6 with "Run_all_stimuli_mp.py"
 Scripts refer to the files with TDT circuits, which are in:
 'C:/Users/InVivo/Desktop/RZ6_Python/'  (.rcx files)
 Created on Wed Dec  8 17:17:36 2021
-@author: MPW
+@author: MPW + JS
 """
 
-
+# here we load the function for the sounds
 import numpy as np
 import wavio
 import matplotlib.pyplot as plt
@@ -17,8 +17,163 @@ import time
 import tdt
 from tqdm import tqdm
 import math
+# %% here we load the function for the screen
 
-#%%
+from psychopy import core
+from ctypes import windll       # load parallel port library
+# from psychopy.hardware import crs
+from psychopy import visual
+from psychopy import monitors
+import time
+import os
+import numpy as np
+from psychopy.visual.windowwarp import Warper
+import nidaqmx      # do not use port0.0, port0.1=4, 2=8, 3=16,...
+
+
+#%% here start the functions for the visual exposure
+#%% here start the functions for the visual exposure
+
+def get_default_parameters():
+    header = {}
+    header['folder_tmp'] = u'\\\\ONLINEANALYSIS\\exchange-folder\\onlinetmp\\'
+    header['folder_save'] = u'\\\\ONLINEANALYSIS\\exchange-folder\\onlinedata\\'
+    
+    monitor = {}
+    monitor['background'] = [0.,0.,0.]
+    monitor['refreshrate'] = 120.
+    
+    # stimulus params
+    stimulus = {}
+    stimulus['onset_adaptation_time_sec'] = 2.
+
+    # analysis
+    analysis = {}
+    
+
+# %%
+class Trigger:
+    def __init__(self):
+#        self.portadress = int('0x0378', 16) 
+#        #self.portadress = int('0x0278', 16) 
+#        self.port = windll.inpoutx64
+#        self.port.Out32(self.portadress,0)
+        task = nidaqmx.Task()
+        task.do_channels.add_do_chan('Dev1/port0/line0:7')
+        task.start()
+        self.task = task
+        
+    def triggger(self,dataout):
+#        duration = 0.002
+#        self.port.Out32(self.portadress,dataout)
+#        core.wait(duration)
+#        self.port.Out32(self.portadress,0)
+        
+        #duration = 0.00001#0.001 #0.1
+        #for i in range(10):
+        self.task.write([dataout])
+        self.task.write([0])
+        
+        
+    def FrameTime(self):
+        #  self.triggger(32)  # before seewiesen
+        self.triggger(4) # pin 2 is 2 in dec 010000 (WARNING BINARY CODE HERE)
+        
+    def Stimulus_Start(self):
+#        self.triggger(1) # pin 1 is 1 dec 100000 (WARNING BINARY CODE HERE)
+        self.triggger(16) #(4) # pin 1 is 1 dec 100000 (WARNING BINARY CODE HERE)    
+                
+    def Stimulus_Stop(self):
+        self.triggger(16) # pin 3 is 4 in dec 001000 (WARNING BINARY CODE HERE)
+
+        
+        
+def OpenScreen(background_color,monitor_distance,monitor_type):
+    ## the fopllowing paragraph is to load the dell monitor high resolution
+
+    if monitor_type == 'Dome':
+        ## functionnal paragraph for the dome set-up in february
+        background_color = [-1,-1,-1]
+        DellMonitor = monitors.Monitor('NEC_20191202good_Seewiesen20200106')# NEC_20191202good_Seewiesen20200106 with Caro 
+        
+        DellMonitor.setDistance(monitor_distance)
+        win= visual.Window(monitor = DellMonitor,screen=0,waitBlanking=True,size=[1280, 800],fullscr=True,allowGUI=False,units='deg',color=background_color, useFBO=True)
+        ## the following line is the used warping for the dome exp.
+        warper14 = Warper(win, warp= 'warpfile', warpfile = 'test_xyuv_20190712_early_js.data', eyepoint = [0.5, 0.5],  flipHorizontal =False, flipVertical =False) # good version from the 12th july 2019
+
+        ## end of the dome paragraph
+        print 'dome options selected'
+    else:
+        ## functionnal paragraph for the Dell screen in the neuropixel set-up
+        background_color = [-1,-1,-1]
+#        DellMonitor = monitors.Monitor('Dell_20190626') # to be updated
+        DellMonitor = monitors.Monitor('Ababax_screen')# ABABAX curved screen
+        print "loading ababax"
+        DellMonitor.setDistance(monitor_distance)
+        win= visual.Window(monitor = DellMonitor,screen=0,waitBlanking=True,size=[2560, 1440],fullscr=False,allowGUI=False,units='deg',color=background_color,winType='pygame')
+#        win= visual.Window(monitor = DellMonitor,screen=0,waitBlanking=True,size=[2560, 1440],fullscr=False,allowGUI=False,units='deg',color=background_color,winType='pygame')
+        print 'dell screen options selected'
+          
+    trigger = Trigger()
+    time.sleep(1.)
+    
+    return win,trigger
+
+
+def present_pause(n_frames,win,trigger=None):
+    stim=visual.TextStim(win)
+    for i in range(n_frames):
+        stim.text = str(n_frames-i)
+        stim.pos = [-3000,0]
+        stim.draw()
+        win.flip()
+#        if trigger is not None:
+#            trigger.EyeCamera()
+
+     
+def write_current_params_and_wait_for_go(params):
+    tmp_dir = params['header']['folder_tmp']
+    filename_tmp = tmp_dir + 'current_params.npy'
+    np.save(filename_tmp,params)
+    print('Saved params')
+    # now we wait for the go
+    filename_go_stimulus = tmp_dir + 'go_stimulus.npy'
+    
+    wait = 1
+    while wait:
+        if os.path.isfile(filename_go_stimulus):
+            # we got the go signals from the online analysis computer!! Hurra!
+            wait = 0
+            # we remove the go signal
+            os.remove(filename_tmp)
+            os.remove(filename_go_stimulus)
+        else:
+            time.sleep(0.5)
+            print('Wait for Go!')
+            
+def generate_filename_and_make_folders(params):
+    # %
+    date = params['header']['date']
+    penetration = params['header']['penetration']
+    label = params['header']['label']
+    stimulus = params['stimulus']['type']
+    
+    #filename_tmp =  params['header']['folder_tmp']+'current_params.npy'
+    
+    folder_save = params['header']['folder_save']+date
+    if not params['test']: # if we test we dont make dirs etc. it just takes time
+        if not os.path.exists(folder_save):
+            os.makedirs(folder_save)
+    
+    filename_save = folder_save+'\\'+penetration+label+stimulus+'_params.npy'
+    
+    return filename_save
+
+
+#%% There starts the functions for the sounds exposure
+
+#%% There starts the functions for the sounds exposure
+#%% There starts the functions for the sounds exposure
 
 '''
 Function to play 333 ms TTL between protocols
@@ -30,7 +185,6 @@ def Between_protocols_TTL():
     for i in range(5):
         crc.trigger('A')
         time.sleep(200 / 1000)    
-        
 
 #%%
 
